02ada61ebef15d4448adee779efe5171b7fc8ff8
王宇轩 <165140760@qq.com>
2019-10-30 12:10:47 +0800

    item11

---
+++ src/main/java/com/wangyuxuan/effectivejava/chapter3/item11/PhoneNumber.java
@@ -0,0 +1,74 @@
+package com.wangyuxuan.effectivejava.chapter3.item11;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author wangyuxuan
+ * @date 2019/10/30 10:01
+ * @description
+ */
+public final class PhoneNumber {
+
+    private final short areaCode, prefix, lineNum;
+
+    public PhoneNumber(int areaCode, int prefix, int lineNum) {
+        this.areaCode = rangeCheck(areaCode, 999, "area code");
+        this.prefix = rangeCheck(prefix, 999, "prefix");
+        this.lineNum = rangeCheck(lineNum, 9999, "line num");
+    }
+
+    private static short rangeCheck(int val, int max, String arg) {
+        if (val < 0 || val > max) {
+            throw new IllegalArgumentException(arg + ": " + val);
+        }
+        return (short) val;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof PhoneNumber)) {
+            return false;
+        }
+        PhoneNumber pn = (PhoneNumber) obj;
+        return pn.areaCode == areaCode && pn.prefix == prefix && pn.lineNum == lineNum;
+    }
+
+//    @Override
+//    public int hashCode() {
+//        int result = Short.hashCode(areaCode);
+//        result = 31 * result + Short.hashCode(prefix);
+//        result = 31 * result + Short.hashCode(lineNum);
+//        return result;
+//    }
+
+
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(lineNum, prefix, areaCode);
+//    }
+
+    // Automatically initialized to 0
+    private int hashCode;
+
+    @Override
+    public int hashCode() {
+        int result = hashCode;
+        if (result == 0) {
+            result = Short.hashCode(areaCode);
+            result = 31 * result + Short.hashCode(prefix);
+            result = 31 * result + Short.hashCode(lineNum);
+            hashCode = result;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Map<PhoneNumber, String> m = new HashMap<>();
+        m.put(new PhoneNumber(707, 867, 5309), "Jenny");
+        System.out.println(m.get(new PhoneNumber(707, 867, 5309)));
+    }
+}
